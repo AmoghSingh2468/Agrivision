bc0689b3f045b10c44d85850bff8df11cdc04ca0
Amogh Singh <asingh18_be23@thapar.edu>
2026-07-17 18:07:57 +0530

    Configure Vercel deployment

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ CORS(app)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-MODEL_PATH = os.path.join(BASE_DIR, "models", "trained_plant_disease_model.keras")'
+MODEL_PATH = os.path.join(BASE_DIR, "models", "trained_plant_disease_model.keras")
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
